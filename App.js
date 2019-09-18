d0b44089f7683dd3bef044942d3b6432c4261a3d
Julian Currie <julian.currie@gmail.com>
2019-09-18 11:43:00 +0700

    Added react-navigation lib. added a playground page for future development

--- App.js
+++ App.js
@@ -1,34 +1,13 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import Home from './src/home'
+import Playground from './src/playground';
 
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Coding with Curry</Text>
-      <Text style={styles.body}>This is the master branch. As you can see, there's not much here.
-      Please checkout the other branches for code related to specific tutorials/videos</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 16,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 32,
-    marginBottom: 32
-  },
-  body: {
-    fontSize: 22
-  }
+const AppStack = createStackNavigator({
+  Home: Home,
+  Playground: Playground
 });
 
-export default App;
+export default createAppContainer(AppStack);
+
+
---
+++ src/home.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button
+} from 'react-native';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Coding with Curry</Text>
+        <Text style={styles.body}>This is the master branch. As you can see, there's not much here.
+      Please checkout the other branches for code related to specific tutorials/videos</Text>
+        <Button
+          title='Go to Playground'
+          onPress={() => { this.props.navigation.navigate('Playground') }} />
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 16,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 32
+  },
+  body: {
+    fontSize: 22
+  }
+});
