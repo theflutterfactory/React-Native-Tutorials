7fe57f44974e008ec14fd197c5736f6dbb9b95e5
Julian Currie <julian.currie@gmail.com>
2019-10-07 23:02:30 +0700

    added necessary changes for react native 0.61+

--- android/app/src/main/java/com/cwc_react_native/MainApplication.java
+++ android/app/src/main/java/com/cwc_react_native/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
