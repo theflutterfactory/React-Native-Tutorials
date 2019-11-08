ecefc35064be211a6643c34ac998cf9e7c7864be
Julian Currie <julian.currie@gmail.com>
2019-11-08 17:24:01 +0700

    Added Code for Part 2

--- src/MapScreen.js
+++ src/MapScreen.js
@@ -4,11 +4,16 @@ import {
   View,
   Text,
   Image,
-  Alert
+  Alert,
+  Platform,
+  Dimensions
 } from 'react-native';
 import MapView,
 { PROVIDER_GOOGLE, Marker, Callout, Polygon, Circle }
   from 'react-native-maps';
+import { request, PERMISSIONS } from 'react-native-permissions';
+import Geolocation from '@react-native-community/geolocation';
+import Carousel from 'react-native-snap-carousel';
 
 export default class Playground extends Component {
 
@@ -17,13 +22,18 @@ export default class Playground extends Component {
   };
 
   state = {
+    markers: [],
     coordinates: [
       { name: 'Burger', latitude: 37.8025259, longitude: -122.4351431, image: require('./img/burger.jpg') },
-      { name: 'Pizza', latitude: 37.7896386, longitude: -122.421646, image: require('./img/pizza.jpg') },
-      { name: 'Soup', latitude: 37.7665248, longitude: -122.4161628, image: require('./img/soup.jpg') },
-      { name: 'Sushi', latitude: 37.7734153, longitude: -122.4577787, image: require('./img/sushi.jpg') },
-      { name: 'Curry', latitude: 37.7948605, longitude: -122.4596065, image: require('./img/curry.jpg') },
+      { name: 'Pizza', latitude: 37.7946386, longitude: -122.421646, image: require('./img/pizza.jpg') },
+      { name: 'Soup', latitude: 37.7665248, longitude: -122.4165628, image: require('./img/soup.jpg') },
+      { name: 'Sushi', latitude: 37.7834153, longitude: -122.4527787, image: require('./img/sushi.jpg') },
+      { name: 'Curry', latitude: 37.7948105, longitude: -122.4596065, image: require('./img/curry.jpg') },
     ]
+  }
+
+  componentDidMount() {
+    this.requestLocationPermission();
   }
 
   showWelcomeMessage = () =>
@@ -40,67 +50,166 @@ export default class Playground extends Component {
         }
       ]
     )
+
+  requestLocationPermission = async () => {
+    if (Platform.OS === 'ios') {
+      var response = await request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+      console.log('iPhone: ' + response);
+
+      if (response === 'granted') {
+        this.locateCurrentPosition();
+      }
+    } else {
+      var response = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+      console.log('Android: ' + response);
+
+      if (response === 'granted') {
+        this.locateCurrentPosition();
+      }
+    }
   }
+
+  locateCurrentPosition = () => {
+    Geolocation.getCurrentPosition(
+      position => {
+        console.log(JSON.stringify(position));
+
+        let initialPosition = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.035
+        }
+
+        this.setState({ initialPosition });
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000 }
+    )
+  }
+
+  onCarouselItemChange = (index) => {
+    let location = this.state.coordinates[index];
+
+    this._map.animateToRegion({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.035
+    })
+
+    this.state.markers[index].showCallout()
+  }
+
+  onMarkerPressed = (location, index) => {
+    this._map.animateToRegion({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: 0.09,
+      longitudeDelta: 0.035
+    });
+
+    this._carousel.snapToItem(index);
+  }
+
+  renderCarouselItem = ({ item }) =>
+    <View style={styles.cardContainer}>
+      <Text style={styles.cardTitle}>{item.name}</Text>
+      <Image style={styles.cardImage} source={item.image} />
+    </View>
 
   render() {
     return (
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation={true}
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.035
-        }}>
+      <View style={styles.container}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          ref={map => this._map = map}
+          showsUserLocation={true}
+          style={styles.map}
+          initialRegion={this.state.initialPosition}>
 
-        <Polygon
-          coordinates={this.state.coordinates}
-          fillColor={'rgba(100, 100, 200, 0.3)'}
+          <Polygon
+            coordinates={this.state.coordinates}
+            fillColor={'rgba(100, 100, 200, 0.3)'}
+          />
+          <Circle
+            center={{ latitude: 37.8025259, longitude: -122.4351431 }}
+            radius={1000}
+            fillColor={'rgba(200, 300, 200, 0.5)'}
+          />
+          <Marker
+            draggable
+            coordinate={{ latitude: 37.7825259, longitude: -122.4351431 }}
+            image={require('./img/sushi.png')}>
+
+            <Callout onPress={this.showWelcomeMessage}>
+              <Text>An Interesting city</Text>
+            </Callout>
+
+          </Marker>
+          {
+            this.state.coordinates.map((marker, index) => (
+              <Marker
+                key={marker.name}
+                ref={ref => this.state.markers[index] = ref}
+                onPress={() => this.onMarkerPressed(marker, index)}
+                coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+              >
+                <Callout>
+                  <Text>{marker.name}</Text>
+                </Callout>
+
+              </Marker>
+            ))
+          }
+
+
+        </MapView>
+        <Carousel
+          ref={(c) => { this._carousel = c; }}
+          data={this.state.coordinates}
+          containerCustomStyle={styles.carousel}
+          renderItem={this.renderCarouselItem}
+          sliderWidth={Dimensions.get('window').width}
+          itemWidth={300}
+          removeClippedSubviews={false}
+          onSnapToItem={(index) => this.onCarouselItemChange(index)}
         />
-        <Circle
-          center={{ latitude: 37.8025259, longitude: -122.4351431 }}
-          radius={1000}
-          fillColor={'rgba(200, 300, 200, 0.5)'}
-        />
-        <Marker
-          draggable
-          coordinate={{ latitude: 37.7825259, longitude: -122.4351431 }}>
-
-          <Callout onPress={this.showWelcomeMessage}>
-            <Image source={require('./img/sushi.png')} />
-            <Text>An Interesting city</Text>
-          </Callout>
-
-          <Image source={require('./img/sushi.png')} />
-
-        </Marker>
-        {
-          this.state.coordinates.map(marker => (
-            <Marker
-              key={marker.name}
-              coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-            >
-              <Callout>
-                <Image
-                  style={{ width: 50, height: 50 }}
-                  source={require('./img/sushi.png')} />
-                <Text>{marker.name}</Text>
-              </Callout>
-
-            </Marker>
-          ))
-        }
-
-
-      </MapView>
+      </View>
     );
   }
-};
+}
 
 const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject
+  },
   map: {
-    height: '100%'
+    ...StyleSheet.absoluteFillObject
+  },
+  carousel: {
+    position: 'absolute',
+    bottom: 0,
+    marginBottom: 48
+  },
+  cardContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    height: 200,
+    width: 300,
+    padding: 24,
+    borderRadius: 24
+  },
+  cardImage: {
+    height: 120,
+    width: 300,
+    bottom: 0,
+    position: 'absolute',
+    borderBottomLeftRadius: 24,
+    borderBottomRightRadius: 24
+  },
+  cardTitle: {
+    color: 'white',
+    fontSize: 22,
+    alignSelf: 'center'
   }
 });
