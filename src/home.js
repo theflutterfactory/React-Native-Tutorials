448b439de089ae67683d7bcbece83ecb012bdfbb
Julian Currie <julian.currie@gmail.com>
2019-12-06 11:52:04 +0700

    created settings screen with local state

--- src/home.js
+++ src/home.js
@@ -2,20 +2,71 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text,
-  Button
+  Switch
 } from 'react-native';
+import { Card, Text, ButtonGroup, Button } from 'react-native-elements';
+import Slider from '@react-native-community/slider';
 
 export default class Home extends Component {
+
+  state = {
+    showNotifications: false,
+    age: 18
+  }
+
+  colors = ['blue', 'green', 'red', 'purple'];
+
   render() {
+    buttons = [
+      { element: () => <Text style={styles.buttonGroupText}>{this.colors[0]}</Text> },
+      { element: () => <Text style={styles.buttonGroupText}>{this.colors[1]}</Text> },
+      { element: () => <Text style={styles.buttonGroupText}>{this.colors[2]}</Text> },
+      { element: () => <Text style={styles.buttonGroupText}>{this.colors[3]}</Text> }
+    ]
+
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Coding with Curry</Text>
-        <Text style={styles.body}>This is the master branch. As you can see, there's not much here.
-      Please checkout the other branches for code related to specific tutorials/videos</Text>
+        <Card containerStyle={styles.card} title='Notifications' >
+          <View style={styles.row}>
+            <Text style={{ fontSize: 16 }}>Show Notifications</Text>
+            <Switch
+              style={{ marginLeft: 16 }}
+              value={this.state.showNotifications}
+              onValueChange={(showNotifications) => {
+                this.setState({ showNotifications });
+                console.log(showNotifications)
+              }}
+            />
+          </View>
+        </Card>
+        <Card containerStyle={styles.card} title='Theme Color' >
+          <ButtonGroup
+            buttonStyle={{ backgroundColor: '#706669' }}
+            buttons={buttons}
+            containerStyle={styles.buttonGroup} />
+        </Card>
+        <Card containerStyle={styles.card} title='Age' >
+          <View style={styles.row}>
+            <Text style={{ fontSize: 18 }} >{this.state.age}</Text>
+            <Slider
+              style={styles.slider}
+              minimumValue={18}
+              maximumValue={65}
+              step={1}
+              value={18}
+              onValueChange={(age) => {
+                this.setState({ age });
+              }}
+            />
+          </View>
+        </Card>
         <Button
-          title='Go to Playground'
-          onPress={() => { this.props.navigation.navigate('Playground') }} />
+          containerStyle={styles.restoreButtonContainer}
+          buttonStyle={{
+            padding: 16,
+          }}
+          title="Restore Defaults"
+        />
       </View>
     );
   }
@@ -23,16 +74,35 @@ export default class Home extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    margin: 16,
+    borderColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center'
+    height: '100%',
+    backgroundColor: '#D0D3D4'
   },
-  title: {
-    fontSize: 32,
-    marginBottom: 32
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
   },
-  body: {
-    fontSize: 22
+  buttonGroup: {
+    height: 60,
+    width: 300
+  },
+  buttonGroupText: {
+    fontSize: 18,
+    color: 'white'
+  },
+  slider: {
+    width: 250,
+    marginLeft: 16
+  },
+  card: {
+    alignItems: 'center',
+    backgroundColor: '#EAECEE',
+    borderColor: '#ABB2B9',
+    borderWidth: 0.3,
+    width: '90%',
+  },
+  restoreButtonContainer: {
+    margin: 32
   }
 });
