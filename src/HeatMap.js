af2f496d5baee2f6c1c8c324a2100bf1e83f0e43
Julian Currie <julian.currie@gmail.com>
2020-03-09 08:02:45 +0700

    added Heatmap component

--- src/HeatMap.js
+++ src/HeatMap.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  View
+  View,
+  Platform
 } from 'react-native';
-import MapView,
-{ PROVIDER_GOOGLE }
-  from 'react-native-maps';
+import MapView, { Heatmap, PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default class HeatMap extends Component {
 
@@ -23,21 +22,47 @@ export default class HeatMap extends Component {
   }
 
   points = [
-    { latitude: 40.7128, longitude: -74.0065, weight: 1 },
-    { latitude: 40.7121, longitude: -74.0042, weight: 1 },
-    { latitude: 40.7102, longitude: -74.0060, weight: 1 },
+    { latitude: 40.7828, longitude: -74.0065, weight: 1 },
+    { latitude: 41.7121, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7102, longitude: -75.0060, weight: 1 },
     { latitude: 40.7123, longitude: -74.0052, weight: 1 },
-    { latitude: 40.7232, longitude: -74.0042, weight: 1 },
-    { latitude: 40.7128, longitude: -74.0024, weight: 1 },
-    { latitude: 40.7223, longitude: -74.0053, weight: 1 },
+    { latitude: 40.7032, longitude: -74.0042, weight: 1 },
+    { latitude: 40.7198, longitude: -74.0024, weight: 1 },
+    { latitude: 41.7223, longitude: -74.0053, weight: 1 },
     { latitude: 40.7181, longitude: -74.0042, weight: 1 },
     { latitude: 40.7124, longitude: -74.0023, weight: 1 },
-    { latitude: 40.7118, longitude: -74.0012, weight: 1 },
-    { latitude: 40.6128, longitude: -74.0127, weight: 1 },
+    { latitude: 40.7648, longitude: -74.0012, weight: 1 },
+    { latitude: 41.7128, longitude: -74.0027, weight: 1 },
     { latitude: 40.7223, longitude: -74.0153, weight: 1 },
     { latitude: 40.7193, longitude: -74.0052, weight: 1 },
-    { latitude: 40.724, longitude: -74.0013, weight: 1 },
-    { latitude: 40.7518, longitude: -74.0412, weight: 1 }
+    { latitude: 40.7241, longitude: -75.0013, weight: 1 },
+    { latitude: 41.7518, longitude: -74.0085, weight: 1 },
+    { latitude: 40.7599, longitude: -74.0093, weight: 1 },
+    { latitude: 41.7523, longitude: -74.0021, weight: 1 },
+    { latitude: 40.7342, longitude: -74.0152, weight: 1 },
+    { latitude: 40.7484, longitude: -75.0042, weight: 1 },
+    { latitude: 40.7929, longitude: -75.0023, weight: 1 },
+    { latitude: 40.7292, longitude: -74.0013, weight: 1 },
+    { latitude: 40.7940, longitude: -74.0048, weight: 1 },
+    { latitude: 40.7874, longitude: -74.0052, weight: 1 },
+    { latitude: 40.7824, longitude: -74.0024, weight: 1 },
+    { latitude: 40.7232, longitude: -74.0094, weight: 1 },
+    { latitude: 41.7342, longitude: -74.0152, weight: 1 },
+    { latitude: 41.7484, longitude: -74.0012, weight: 1 },
+    { latitude: 41.7929, longitude: -74.0073, weight: 1 },
+    { latitude: 41.7292, longitude: -74.0013, weight: 1 },
+    { latitude: 41.7940, longitude: -74.0058, weight: 1 },
+    { latitude: 41.7874, longitude: -74.0352, weight: 1 },
+    { latitude: 41.7824, longitude: -74.0024, weight: 1 },
+    { latitude: 41.7232, longitude: -74.0094, weight: 1 },
+    { latitude: 41.0342, longitude: -75.0152, weight: 1 },
+    { latitude: 41.0484, longitude: -75.0012, weight: 1 },
+    { latitude: 41.0929, longitude: -75.0073, weight: 1 },
+    { latitude: 41.0292, longitude: -74.0013, weight: 1 },
+    { latitude: 41.0940, longitude: -74.0068, weight: 1 },
+    { latitude: 41.0874, longitude: -74.0052, weight: 1 },
+    { latitude: 41.0824, longitude: -74.0024, weight: 1 },
+    { latitude: 41.0232, longitude: -74.0014, weight: 1 }
   ];
 
   render() {
@@ -48,6 +73,18 @@ export default class HeatMap extends Component {
           ref={map => this._map = map}
           style={styles.map}
           initialRegion={this.state.initialPosition}>
+          <Heatmap
+            points={this.points}
+            radius={40}
+            opacity={1}
+            gradient={{
+              colors: ["black", "purple", "red", "orange", "white"],
+              startPoints: Platform.OS === 'ios' ? [0.01, 0.04, 0.1, 0.45, 0.5] :
+                [0.1, 0.25, 0.5, 0.75, 1],
+              colorMapSize: 2000
+            }}
+          >
+          </Heatmap>
         </MapView>
       </View>
     );
