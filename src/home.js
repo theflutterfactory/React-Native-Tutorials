580b3a8f70c9e08e3667b73941ba328f08eeae7c
Julian Currie <julian.currie@gmail.com>
2020-06-21 19:12:24 +0700

    Added a CWC logo on the home screen

--- src/home.js
+++ src/home.js
@@ -3,13 +3,18 @@ import {
   StyleSheet,
   View,
   Text,
-  Button
+  Button,
+  Image
 } from 'react-native';
 
 export default class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Image
+          source={require('./assets/cc_logo.png')}
+          style={styles.image}
+        />
         <Text style={styles.title}>Coding with Curry</Text>
         <Text style={styles.body}>This is the master branch. As you can see, there's not much here.
       Please checkout the other branches for code related to specific tutorials/videos</Text>
@@ -30,9 +35,14 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 32,
-    marginBottom: 32
+    marginBottom: 32,
+    marginTop: 32
   },
   body: {
     fontSize: 22
+  },
+  image: {
+    height: 100,
+    width: 100
   }
 });
