bf2fa3e3c070688485c85e1a64cac42f47f07553
Julian Currie <julian.currie@gmail.com>
2020-06-21 18:45:36 +0700

    updated React Navigation to v5, updated other pkgs

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/home'
 import Playground from './src/playground';
 
-const AppStack = createStackNavigator({
-  Home: Home,
-  Playground: Playground
-});
+const Stack = createStackNavigator();
 
-export default createAppContainer(AppStack);
-
+export default App = () =>
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Playground" component={Playground} />
+    </Stack.Navigator>
+  </NavigationContainer>
 
