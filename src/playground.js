f7dec8d3caafd5c5833d37821571e3821fd268d8
julian_currie <julian.currie@pushfit.tv>
2020-11-17 22:25:46 +0700

    Added finished code

--- src/playground.js
+++ src/playground.js
@@ -1,16 +1,46 @@
-import React from 'react';
-import { Title } from './styles/text'
-import { Container } from './styles/wrapper'
-import { RoundedInput } from './styles/input'
-import { RoundedButton } from './styles/buttons'
+import React, { useState } from 'react';
+import { WebView } from 'react-native-webview';
+import * as Progress from 'react-native-progress';
 
-export default Playground = () =>
-  <Container>
-    <Title>Login</Title>
-    <RoundedInput placeholder='Username' />
-    <RoundedInput password placeholder='Password' />
-    <RoundedButton
-      title='SUBMIT'
-      width={'80%'}
+export default Playground = () => {
+  const [progress, setProgress] = useState(0);
+  const [isLoaded, setLoaded] = useState(false);
+
+  const source = Platform.OS === 'ios' ? require('./assets/sample.html')
+    : { uri: 'file:///android_asset/sample.html' };
+
+  const javascript = `
+  document.body.style.backgroundColor = 'orange';
+  window.alert('This is javascript');
+`;
+
+  return <>
+    {
+      !isLoaded ?
+        <Progress.Bar
+          progress={progress}
+          width={null}
+          borderWidth={0}
+          borderRadius={0}
+          color='#ff8300'
+        /> : null
+    }
+    <WebView
+      source={{
+        uri: 'https://google.com',
+        headers: { 'key': 'value' }
+      }}
+      // source={{ html: '<h1>This is a statsampleic HTML source!</h1>' }}
+      // source={source}
+      onError={(event) =>
+        alert(`Webview error: ${event.nativeEvent.description}`)
+      }
+      // injectedJavaScript={javascript}
+      onMessage={(event) => {
+        alert(event.nativeEvent.data);
+      }}
+      onLoadProgress={({ nativeEvent }) => setProgress(nativeEvent.progress)}
+      onLoadEnd={() => setLoaded(true)}
     />
-  </Container>
+  </>
+}
